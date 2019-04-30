55dd42e7d0ad195e6aee7cb2fd27db95fb09c687
gazor117 <oreillygary7@gmail.com>
2019-04-30 23:05:25 +0100

    Ship follow working

--- SpaceBattle/Assets/FollowShip.cs
+++ SpaceBattle/Assets/FollowShip.cs
@@ -5,37 +5,67 @@ using UnityEngine;
 
 public class FollowShip : MonoBehaviour
 {
+    //Decides whether it picks a ship from the UNSC arraylist or the CVNT arraylist
     [Tooltip("Select if following UNSC Ships, leave as false for Covenant")]
     public bool UNSCShips;
 
     public GameObject Waypoint;
     
     GameObject GameManager;
+    [SerializeField]
     private List<GameObject> ShipRefs;
 
     private Transform target;
 
-    public bool testFindShip;
+    private bool testFindShip;
 
     private bool battleBegun;
+
+    public Vector3 offset;
+    private GameObject Spawner;
     
     
     // Start is called before the first frame update
     void Start()
     {
         GameManager = GameObject.FindGameObjectWithTag("GM");
+        //Chooses which array and spawner to use
         ShipRefs = UNSCShips ? GameManager.GetComponent<ShipManager>().UNSCShips : GameManager.GetComponent<ShipManager>().CVNTShips;
-        battleBegun = Waypoint.GetComponent<ShipArrived>().battleBegun;
+        //battleBegun = Waypoint.GetComponent<ShipArrived>().battleBegun;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (testFindShip && battleBegun )
+//        Debug.Log(battleBegun);
+        battleBegun = Waypoint.GetComponent<ShipArrived>().battleBegun;
+        //if all ships are spawned
+        
+        if (battleBegun)
         {
-            FindShip();
-            GetComponent<CinemachineVirtualCamera>().Follow = target;
-            testFindShip = false;
+            Spawner = UNSCShips ? GameObject.FindWithTag("UNSCSpawner") : GameObject.FindWithTag("CVNTSpawner");
+            if (ShipRefs.Count == Spawner.GetComponent<ShipSpawning>().ShipAmount && testFindShip == false)
+            {
+                FindShip();
+                //target.position += offset;
+                GetComponent<CinemachineVirtualCamera>().Follow = target;
+                testFindShip = true;
+            }
+
+            if (testFindShip)
+            {
+               
+
+
+                   
+                
+            }
+
+            if (target != null)
+            {
+                transform.rotation = target.rotation;
+            }
         }
     }
 
@@ -43,5 +73,6 @@ public class FollowShip : MonoBehaviour
     {
         int ran = Random.Range(0, ShipRefs.Count);
         target = ShipRefs[ran].transform;
+        
     }
 }
