cd14dd075e2855fc3beef7cac9e12266c1e3cc50
gazor117 <oreillygary7@gmail.com>
2019-04-30 10:26:49 +0100

    FindShip

---
+++ SpaceBattle/Assets/FindShip.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindShip : MonoBehaviour
+{
+    private GameObject GM;
+
+    private List<GameObject> UNSCShips;
+
+    private List<GameObject> CVNTShips;
+    [SerializeField]
+    private List<GameObject> AllShips;
+
+    public GameObject Waypoint;
+
+    private bool battleBegun;
+
+    private int shipAmount;
+
+    private bool arrayNotPopulated = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GameObject.FindWithTag("GM");
+        
+        battleBegun = Waypoint.GetComponent<ShipArrived>().battleBegun;
+//        StartCoroutine(CheckBattleStarted());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       CheckBattleStarted();
+    }
+
+    void PopulateArray()
+    {
+        
+        AllShips.AddRange(UNSCShips);
+        AllShips.AddRange(CVNTShips);
+        arrayNotPopulated = false;
+    }
+
+    void CheckBattleStarted()
+    {
+        if (arrayNotPopulated)
+        {
+            battleBegun = Waypoint.GetComponent<ShipArrived>().battleBegun;
+            CVNTShips = GM.GetComponent<ShipManager>().CVNTShips;
+            UNSCShips = GM.GetComponent<ShipManager>().UNSCShips;
+            if (battleBegun)
+            {
+                shipAmount = GameObject.FindWithTag("UNSCSpawner").GetComponent<ShipSpawning>().ShipAmount;
+                if (shipAmount == UNSCShips.Count)
+                {
+                    PopulateArray();
+                }
+
+            }
+            
+        }
+        
+    }
+}
