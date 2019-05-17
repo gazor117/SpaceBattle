5020cb3606fea24d8436f4e0216d293ac9c3c620
gazor117 <oreillygary7@gmail.com>
2019-05-17 21:33:14 +0100

    Boid commented

--- SpaceBattle/Assets/Scripts/Boid.cs
+++ SpaceBattle/Assets/Scripts/Boid.cs
@@ -37,7 +37,7 @@ public class Boid : MonoBehaviour
 
     public Vector3 SeekForce(Vector3 target)
     {
-        Vector3 desired = target - transform.position;
+        Vector3 desired = target - transform.position;                
         desired.Normalize();
         //Debug.Log(desired);
         //Debug.Log(velocity);
@@ -48,17 +48,17 @@ public class Boid : MonoBehaviour
 
     public Vector3 ArriveForce(Vector3 target, float slowingDistance = 15.0f)
     {
-        Vector3 toTarget = target - transform.position;
+        Vector3 toTarget = target - transform.position;                //Get direction Vector to target
 
-        float distance = toTarget.magnitude;
-        if (distance < 0.1f)
+        float distance = toTarget.magnitude;                            //Get distance to target
+        if (distance < 0.1f)                                            //If very close force = 0
         {
             return Vector3.zero;
         }
-        float ramped = maxSpeed * (distance / slowingDistance);
+        float ramped = maxSpeed * (distance / slowingDistance);         //Only matters if distance < slowingDist, ramped = a speed float
 
-        float clamped = Mathf.Min(ramped, maxSpeed);
-        Vector3 desired = clamped * (toTarget / distance);
+        float clamped = Mathf.Min(ramped, maxSpeed);                    //clamped is equal to the smaller float
+        Vector3 desired = clamped * (toTarget / distance);                //clamped = newSpeed, this is * (toTarget/distnace) i think is normalised
 
         return desired - velocity;
     }
@@ -79,40 +79,40 @@ public class Boid : MonoBehaviour
 
         foreach (SteeringBehaviour b in behaviours)
         {
-            if (b.isActiveAndEnabled)
+            if (b.isActiveAndEnabled)        //if the steering behaviour is active
             {
-                force += b.Calculate() * b.weight;
+                force += b.Calculate() * b.weight;        //Run calculate on the SB and return the force, then multiply this by the weight of on the SB. This means all steering Behaviour forces are added together
 
                 float f = force.magnitude;
-                if (f >= maxForce)
+                if (f >= maxForce)                    //if force is greater than maxforce
                 {
-                    force = Vector3.ClampMagnitude(force, maxForce);
+                    force = Vector3.ClampMagnitude(force, maxForce);                //If force is greater than maxForce, clamp it to maxForce
                     break;
                 }               
             }
         }
 
-        return force;
+        return force;             //return the sum of all behaviours forces after * by their weight, and checking if force is greater than maxForce
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        force = Calculate();
-        Vector3 newAcceleration = force / mass;
-        acceleration = Vector3.Lerp(acceleration, newAcceleration, Time.deltaTime);
-        velocity += acceleration * Time.deltaTime;
-
-        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        force = Calculate();                                            // Calculate the force applied to the ship based on the steering behaviours active
+        Vector3 newAcceleration = force / mass;                            //The new acceleration is got by dividing the force by the mass of the ship
+        acceleration = Vector3.Lerp(acceleration, newAcceleration, Time.deltaTime);    //the old acceleration will move up to the new acceleration over the time of one frame
+        velocity += acceleration * Time.deltaTime;                        // Add the acceleration multiplied by the frametime to the velocity to get the new velocity
+ 
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);             // If the velocity magnitude is greater than max speed clamp it to max speed
         
-        if (velocity.magnitude > float.Epsilon)
+        if (velocity.magnitude > float.Epsilon)                             //If moving
         {
-            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * banking), Time.deltaTime * 3.0f);
-            transform.LookAt(transform.position + velocity, tempUp);
+            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * banking), Time.deltaTime * 3.0f);        //lerp the current up vector to the Vector3.up + the acceleration vector (where it will move in the next frame) and multiplied by the banking constant.
+            transform.LookAt(transform.position + velocity, tempUp);                                //Rotate the ship to look at the next position it will move and align its up vector to the tempUp vector
 
-            transform.position += velocity * Time.deltaTime;
-            velocity *= (1.0f - (damping * Time.deltaTime));
+            transform.position += velocity * Time.deltaTime;                    //Add velocity vector to the position
+            velocity *= (1.0f - (damping * Time.deltaTime));                    //multiply velocity vector by (1 - damping value)
         }
     }
 }
